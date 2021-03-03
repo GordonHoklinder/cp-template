447d15e075ae7cf08d1245b667ebe6e253cdcb0c
GordonHoklinder <49371953+GordonHoklinder@users.noreply.github.com>
2021-03-03 18:40:32 +0100

    Initial

---
+++ template.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef pair<int, int> pii;
+typedef vector<pii> vpi;
+typedef queue<int> qi;
+typedef queue<pii> qpi;
+#define f(i, a, b) for(int i = (a); i < (b); i++)
+#define bf(i, a, b) for(int i = (b) - 1; i >= (a); i--)
+#define all(x) x.begin(), x.end()
+#define pb emplace_back
+#define fi first
+#define se second
+#define swap(a,b) {__typeof(a) temp = a; a = b; b = temp;}
+#define gcd(a,b) __gcd(a,b)
+#define popcount(x) __builtin_popcount(x)
+#define dump(x) cerr << #x << " = " << x << "\n"
+#define ps(x) cout << x << " "
+#define pl(x) cout << x << "\n"
+#define pans(x) {if(x) pl("YES"); else pl("NO");}
+#define mod1 1000000007LL
+#define mod2 998244353LL
+#define mod mod1
+
+signed main()
+{
+	ios_base::sync_with_stdio(0);
+	int cou;
+	//cin >> cou;
+	cou = 1;
+	f(coui, 0, cou)
+	{
+
+	}
+	return 0;
+}
